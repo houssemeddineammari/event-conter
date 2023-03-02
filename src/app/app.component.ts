3f1676c51ee12b9206cf4a30d76e93eb733f9c8a
Your Name <you@example.com>
2023-03-03 00:06:14 +0100

    Creating the counter component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coding-game';
+
+  handleTick(val: any) {
+    console.log('-----TIK-------');
+    console.log(val);
+  }
 }
---
+++ src/app/components/counter/counter.component.ts
@@ -0,0 +1,53 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'counter-component',
+  template: `
+    <div style="text-align: center;
+    padding-top: 100px;
+    height: 100px;
+    background: aliceblue;
+    border-radius: 5px;">
+      <input id="intervalInput" [(ngModel)]="intervalValue" (ngModelChange)="onIntervalChange()">
+      <button id="intervalSetButton" (click)="setInterval()">Set Interval</button>
+    </div>
+  `,
+})
+export class CounterComponent implements OnInit {
+
+  @Input() message: string = '';
+  @Output() tick: EventEmitter<string> = new EventEmitter();
+
+  intervalValue: number = 0;
+  intervalId: any = null;
+
+  ngOnInit() {
+  }
+
+  onIntervalChange() {
+    this.clearTimer();
+    this.startTimer();
+  }
+
+  setInterval() {
+    this.clearTimer();
+    this.startTimer();
+  }
+
+  startTimer() {
+    if(this.intervalValue) {
+      this.intervalId = setInterval(() => {
+        this.tick.emit(this.message);
+      }, this.intervalValue);
+    }
+  }
+
+  clearTimer() {
+    clearInterval(this.intervalId);
+  }
+
+  // This is useful to clear the timer when leaving the component by charging another comp or changing the route
+  ngOnDestroy() {
+    this.clearTimer();
+  }
+}
